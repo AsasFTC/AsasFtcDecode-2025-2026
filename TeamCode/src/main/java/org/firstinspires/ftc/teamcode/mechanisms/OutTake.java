95cce10b718253a16f37b1ecbb77aace4dd3c62b
Equipe Asas FTC <equipe.asas.ftc@gmail.com>
2026-05-09 13:30:19 -0300

    Atualizando pacotes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/OutTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/OutTake.java
@@ -7,43 +7,56 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OutTake {
 
-    DcMotorEx flywheel;
+    DcMotorEx flywheel, flywheel2;
 
 
     public void init(HardwareMap hwmap){
         flywheel = (DcMotorEx) hwmap.get(DcMotor.class, "flywheel");
+        flywheel2 = (DcMotorEx) hwmap.get(DcMotor.class, "flywheel2");
 
+        flywheel2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
+
     }
 
-    public double getCurrent(){
-        return flywheel.getCurrent(CurrentUnit.AMPS);
+    public String getCurrent(){
+        List<Double> info = new ArrayList<>();
+        info.add(flywheel.getCurrent(CurrentUnit.AMPS));
+        info.add(flywheel2.getCurrent(CurrentUnit.AMPS));
+        return info.toString();
     }
-    public double getPower(){
-        return flywheel.getPower();
+    public String getPower(){
+        List<Double> info = new ArrayList<>();
+        info.add(flywheel.getPower());
+        info.add(flywheel2.getPower());
+        return info.toString();
     }
-    public double getVelocity(){
-        return flywheel.getVelocity();
+    public String getVelocity(){
+        List<Double> info = new ArrayList<>();
+        info.add(flywheel.getVelocity());
+        info.add(flywheel2.getVelocity());
+        return info.toString();
     }
 
     public void setPower(double power){
         flywheel.setPower(power);
+        flywheel2.setPower(power);
     }
     public void setVelocity(int velocity){
         flywheel.setVelocity(velocity);
+        flywheel2.setVelocity(velocity);
     }
 
     public void turnOff(){
         flywheel.setPower(0);
+        flywheel2.setPower(0);
     }
-
-    public void setDefaultPower(){
-        flywheel.setPower(0.8);
-    }
-
-
 
 }
